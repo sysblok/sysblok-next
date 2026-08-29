6a60626eb6db10800b72ef5bae33a45cc2e0ea8d
tabarzin <tabarzin@gmail.com>
2026-08-29 16:44:33 +0300

    refactor: change footer color

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,15 +14,11 @@ const textAreaClasses =
   '[&>div+div]:mt-6 ' +
   headingClasses +
   ' ' +
-  '[&_a]:text-muted-foreground [&_a:hover]:text-foreground [&_a:hover]:underline [&_a]:underline-offset-4 ' +
+  '[&_a]:underline-offset-4 ' +
   '[&_a]:leading-loose ' +
-  '[&_address]:not-italic [&_address]:text-muted-foreground [&_address]:leading-loose'
+  '[&_address]:not-italic [&_address]:leading-loose'
 
-const inlineLinksClasses =
-  headingClasses +
-  ' ' +
-  '[&_div]:leading-loose [&_div]:text-muted-foreground ' +
-  '[&_a:hover]:text-foreground [&_a:hover]:underline [&_a]:underline-offset-4'
+const inlineLinksClasses = headingClasses
 
 const Footer = async () => {
   const areas = await getFooter()
